099b86bf51d7f58225c49098da845ba24b31085d
Wratten <daniel.wratten@gmail.com>
2021-12-28 13:06:55 +0800

    Wrote pseudocode for app.js

---
+++ assets/app.js
@@ -0,0 +1,27 @@
+// Declare all elements
+
+// When user opens page
+
+// have only the header, code quiz title, short description and begin quiz buttons displaying
+
+// When user clicks begin quiz, start timer, display question and answer buttons
+
+// Create a variable/s holding an array of questions
+
+// Create a variable/s holding an array of possible answers to said questions
+
+// If a user clicks an incorrect answer button
+
+// subtract time from the clock
+
+// When all questions are answered or timer reaches 0
+
+// display end screen containing
+
+// total score
+
+// enter initials form
+
+// submit button
+
+// go back to start button?
